14a5d82d0820d2c9feb63fec4a771fb98e13bd42
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2024-09-26 14:15:16 +0300

    improve fib algs iterative & binary

--- fib_algs.cpp
+++ fib_algs.cpp
@@ -1,4 +1,5 @@
 #include "fib_algs.h"
+
 using namespace std;
 
 long int iteration_recursive = 0;
@@ -6,6 +7,7 @@ long int iteration_iterative = 0;
 long int iteration_binary = 0;
 
 long long fib_recursive(int n) { // O(2^n)
+
     if (n < 2) {
         iteration_recursive++;
         return n;
@@ -16,32 +18,39 @@ long long fib_recursive(int n) { // O(2^n)
 }
 
 long long fib_iterative(int n) { // O(n)
-    int prev_fib = 0, current_fib = 1;
-    for (int i = 0; i <= n; i++) {
-        current_fib = prev_fib + current_fib;
-        prev_fib = current_fib - prev_fib;
-        iteration_iterative += 4;
+
+    int i = 1, j = 0;
+    for (int k = 1; k < n; k++) {
+        j = i + j;
+        i = j - i;
+        iteration_iterative++;
     }
-    return current_fib;
+    return j;
 }
 
-long long fib_binary(int n) { // O(log n)
-    int next_fib_value = 1, fib_n = 0, temp1 = 0, temp2 = 1, intermediate;
-    iteration_binary += 4;
+long long fib_binary(int n) {
+
+    int i, j, k, h, t;
+    i = 1;
+    j = 0;
+    k = 0;
+    h = 1;
+
     while (n > 0) {
-        if (n % 2 != 0) {
-            intermediate = fib_n * temp2;
-            fib_n = next_fib_value * temp2 + fib_n * temp1 + intermediate;
-            next_fib_value = next_fib_value * temp1 + intermediate;
-            iteration_binary += 11;
+        iteration_binary++;
+        if (n % 2 == 0) {
+            t = j * h;
+            j = i * h + j * k + t;
+            i = i * k + t;
         }
-        intermediate = temp2 * temp2;
-        temp2 = 2 * temp1 * temp2 + intermediate;
-        temp1 = temp1 * temp1 + intermediate;
+
+        t = h * h;
+        h = 2 * k * h + t;
+        k + k * k + t;
+        k = k * k + t;
         n = n / 2;
-        iteration_binary += 11;
     }
-    return fib_n;
+    return j;
 }
 
 
